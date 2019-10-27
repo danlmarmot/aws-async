696beba67d9328c6c19f77b2dc741bb4a2dcd01e
danlmarmot <ilm3Github>
2019-10-27 16:29:55 -0700

    blackened

--- ec2_get_security_groups_async.py
+++ ec2_get_security_groups_async.py
@@ -1,7 +1,7 @@
 import requests
 
 import asyncio
-from aiohttp import ClientSession
+import aiohttp
 
 import datetime
 import hashlib
@@ -12,10 +12,10 @@ import xml.etree.ElementTree as ET
 import os
 import sys
 
-ACCESS_KEY = os.environ.get('AWS_ACCESS_KEY_ID')
-SECRET_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+ACCESS_KEY = os.environ.get("AWS_ACCESS_KEY_ID")
+SECRET_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
 if ACCESS_KEY is None or SECRET_KEY is None:
-    print('No access key is available.')
+    print("No access key is available.")
     sys.exit()
 
 EC2_ENDPOINT = "https://ec2.amazonaws.com/"
@@ -29,6 +29,10 @@ EC2_ENDPOINT = "https://ec2.amazonaws.com/"
 #   AWS example in Python on how to create a signed request
 #   In general, AWS recommends using POST rather than GET.
 
+# Global session for aiohttp, maintains connection pool, ete etc
+_session = None
+
+
 def main():
     region_names = ec2_get_region_names_async()
     print(region_names)
@@ -38,32 +42,38 @@ def ec2_get_region_names_sync():
     # https://docs.aws.amazon.com/AWSEC2/latest/APIReference/API_DescribeRegions.html
     # https://ec2.amazonaws.com/?Action=DescribeRegions
 
-    query = {
-        'Action': 'DescribeRegions',
-        'Version': '2013-10-15'
-    }
+    query = {"Action": "DescribeRegions", "Version": "2013-10-15"}
     # ensure sort order is correct
     query = {k: query[k] for k in sorted(query)}
 
-    query_str = '&'.join([f'{key}={value}' for key, value in query.items()])
+    query_str = "&".join([f"{key}={value}" for key, value in query.items()])
 
-    headers = aws_sig_v4_headers(ACCESS_KEY, SECRET_KEY, {},
-                                 'ec2', 'us-east-1',
-                                 host='ec2.amazonaws.com',
-                                 method="POST",
-                                 path='/',
-                                 query=query,
-                                 payload='')
+    headers = aws_sig_v4_headers(
+        ACCESS_KEY,
+        SECRET_KEY,
+        {},
+        "ec2",
+        "us-east-1",
+        host="ec2.amazonaws.com",
+        method="POST",
+        path="/",
+        query=query,
+        payload="",
+    )
 
-    request_url = EC2_ENDPOINT + '?' + query_str
+    request_url = EC2_ENDPOINT + "?" + query_str
     response = requests.post(request_url, headers=headers)
 
     # parse the XML for region names; note the XML namespace
     region_xml = ET.fromstring(response.text)
-    region_names = sorted(
-        [item.text for item in region_xml.iter('{http://ec2.amazonaws.com/doc/2013-10-15/}regionName')])
+    region_names = [
+        item.text
+        for item in region_xml.iter(
+            "{http://ec2.amazonaws.com/doc/2013-10-15/}regionName"
+        )
+    ]
 
-    return region_names
+    return sorted(region_names)
 
 
 def ec2_get_region_names_async():
@@ -71,95 +81,130 @@ def ec2_get_region_names_async():
 
     return result
 
+
 async def ec2_get_region_names_aiohttp():
-    query = {
-        'Action': 'DescribeRegions',
-        'Version': '2013-10-15'
-    }
+    query = {"Action": "DescribeRegions", "Version": "2013-10-15"}
     # ensure sort order is correct
     query = {k: query[k] for k in sorted(query)}
 
-    query_str = '&'.join([f'{key}={value}' for key, value in query.items()])
+    query_str = "&".join([f"{key}={value}" for key, value in query.items()])
 
-    headers = aws_sig_v4_headers(ACCESS_KEY, SECRET_KEY, {},
-                                 'ec2', 'us-east-1',
-                                 host='ec2.amazonaws.com',
-                                 method="POST",
-                                 path='/',
-                                 query=query,
-                                 payload='')
+    headers = aws_sig_v4_headers(
+        ACCESS_KEY,
+        SECRET_KEY,
+        {},
+        "ec2",
+        "us-east-1",
+        host="ec2.amazonaws.com",
+        method="POST",
+        path="/",
+        query=query,
+        payload="",
+    )
 
-    request_url = EC2_ENDPOINT + '?' + query_str
+    request_url = EC2_ENDPOINT + "?" + query_str
 
-    async with ClientSession() as session:
-        response = await session.post(request_url, headers=headers)
-        response_text = await response.text()
+    # async with ClientSession() as session:
+    session = await get_session()
+    response = await session.post(request_url, headers=headers)
+    response_text = await response.text()
 
     # parse the XML for region names; note the XML namespace
     region_xml = ET.fromstring(response_text)
     region_names = sorted(
-        [item.text for item in region_xml.iter('{http://ec2.amazonaws.com/doc/2013-10-15/}regionName')])
+        [
+            item.text
+            for item in region_xml.iter(
+                "{http://ec2.amazonaws.com/doc/2013-10-15/}regionName"
+            )
+        ]
+    )
 
     return region_names
 
 
-def aws_sig_v4_headers(access_key_id, secret_access_key, pre_auth_headers,
-                       service, region, host, method, path, query, payload):
+def aws_sig_v4_headers(
+    access_key_id,
+    secret_access_key,
+    pre_auth_headers,
+    service,
+    region,
+    host,
+    method,
+    path,
+    query,
+    payload,
+):
     # from https://charemza.name/blog/posts/aws/python/you-might-not-need-boto-3/
-    algorithm = 'AWS4-HMAC-SHA256'
+    algorithm = "AWS4-HMAC-SHA256"
 
     now = datetime.datetime.utcnow()
-    amzdate = now.strftime('%Y%m%dT%H%M%SZ')
-    datestamp = now.strftime('%Y%m%d')
-    payload_hash = hashlib.sha256(payload.encode('utf-8')).hexdigest()
-    credential_scope = f'{datestamp}/{region}/{service}/aws4_request'
+    amzdate = now.strftime("%Y%m%dT%H%M%SZ")
+    datestamp = now.strftime("%Y%m%d")
+    payload_hash = hashlib.sha256(payload.encode("utf-8")).hexdigest()
+    credential_scope = f"{datestamp}/{region}/{service}/aws4_request"
 
     pre_auth_headers_lower = {
-        header_key.lower(): ' '.join(header_value.split())
+        header_key.lower(): " ".join(header_value.split())
         for header_key, header_value in pre_auth_headers.items()
     }
     required_headers = {
-        'host': host,
-        'x-amz-content-sha256': payload_hash,
-        'x-amz-date': amzdate,
+        "host": host,
+        "x-amz-content-sha256": payload_hash,
+        "x-amz-date": amzdate,
     }
     headers = {**pre_auth_headers_lower, **required_headers}
     header_keys = sorted(headers.keys())
-    signed_headers = ';'.join(header_keys)
+    signed_headers = ";".join(header_keys)
 
     def signature():
         def canonical_request():
-            canonical_uri = urllib.parse.quote(path, safe='/~')
+            canonical_uri = urllib.parse.quote(path, safe="/~")
             quoted_query = sorted(
-                (urllib.parse.quote(key, safe='~'), urllib.parse.quote(value, safe='~'))
+                (urllib.parse.quote(key, safe="~"), urllib.parse.quote(value, safe="~"))
                 for key, value in query.items()
             )
-            canonical_querystring = '&'.join(f'{key}={value}' for key, value in quoted_query)
-            canonical_headers = ''.join(f'{key}:{headers[key]}\n' for key in header_keys)
+            canonical_querystring = "&".join(
+                f"{key}={value}" for key, value in quoted_query
+            )
+            canonical_headers = "".join(
+                f"{key}:{headers[key]}\n" for key in header_keys
+            )
 
-            return f'{method}\n{canonical_uri}\n{canonical_querystring}\n' + \
-                   f'{canonical_headers}\n{signed_headers}\n{payload_hash}'
+            return (
+                f"{method}\n{canonical_uri}\n{canonical_querystring}\n"
+                + f"{canonical_headers}\n{signed_headers}\n{payload_hash}"
+            )
 
         def sign(key, msg):
-            return hmac.new(key, msg.encode('utf-8'), hashlib.sha256).digest()
+            return hmac.new(key, msg.encode("utf-8"), hashlib.sha256).digest()
 
-        string_to_sign = f'{algorithm}\n{amzdate}\n{credential_scope}\n' + \
-                         hashlib.sha256(canonical_request().encode('utf-8')).hexdigest()
+        string_to_sign = (
+            f"{algorithm}\n{amzdate}\n{credential_scope}\n"
+            + hashlib.sha256(canonical_request().encode("utf-8")).hexdigest()
+        )
 
-        date_key = sign(('AWS4' + secret_access_key).encode('utf-8'), datestamp)
+        date_key = sign(("AWS4" + secret_access_key).encode("utf-8"), datestamp)
         region_key = sign(date_key, region)
         service_key = sign(region_key, service)
-        request_key = sign(service_key, 'aws4_request')
+        request_key = sign(service_key, "aws4_request")
         return sign(request_key, string_to_sign).hex()
 
     return {
         **pre_auth_headers,
-        'x-amz-date': amzdate,
-        'x-amz-content-sha256': payload_hash,
-        'Authorization': f'{algorithm} Credential={access_key_id}/{credential_scope}, '
-                         f'SignedHeaders={signed_headers}, Signature=' + signature(),
+        "x-amz-date": amzdate,
+        "x-amz-content-sha256": payload_hash,
+        "Authorization": f"{algorithm} Credential={access_key_id}/{credential_scope}, "
+        f"SignedHeaders={signed_headers}, Signature=" + signature(),
     }
 
 
-if __name__ == '__main__':
+async def get_session():
+    global _session
+    if _session is None:
+        _session = aiohttp.ClientSession()
+    return _session
+
+
+if __name__ == "__main__":
     main()
